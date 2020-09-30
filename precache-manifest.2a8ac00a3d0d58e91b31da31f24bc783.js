a1ad6c99dfab0260a769bf2b712c8092f4be02dc
TriP22 <60031832+TriP22@users.noreply.github.com>
2020-09-30 18:44:14 +0530

    Updates

---
+++ precache-manifest.2a8ac00a3d0d58e91b31da31f24bc783.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26a2f36f21d171a53bd72f6190a4722",
+    "url": "/lr-work/index.html"
+  },
+  {
+    "revision": "cc56704be0a9cdba40e0",
+    "url": "/lr-work/static/css/2.7d51b4f0.chunk.css"
+  },
+  {
+    "revision": "05c07a6d57f84aefb6fd",
+    "url": "/lr-work/static/css/main.35e8a0a3.chunk.css"
+  },
+  {
+    "revision": "cc56704be0a9cdba40e0",
+    "url": "/lr-work/static/js/2.e3691573.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lr-work/static/js/2.e3691573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c07a6d57f84aefb6fd",
+    "url": "/lr-work/static/js/main.49ae2207.chunk.js"
+  },
+  {
+    "revision": "2ee5a30c106326df4b76",
+    "url": "/lr-work/static/js/runtime-main.3b2654f6.js"
+  },
+  {
+    "revision": "a47be846018da08931e15051de597f20",
+    "url": "/lr-work/static/media/logo.a47be846.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lr-work/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lr-work/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lr-work/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lr-work/static/media/slick.f97e3bbf.svg"
+  }
+]);
